8575ea7698784841a30069604905fa1fe16ad122
Fikri Haikal <fikrihaikal571@gmail.com>
2023-03-30 14:38:59 +0700

    Fikri Haikal - BottomNavigation

---
+++ app/src/main/java/com/example/latihan_bottomnavigation/FragmentThird.kt
@@ -0,0 +1,20 @@
+package com.example.latihan_bottomnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class FragmentThird : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/latihan_bottomnavigation/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.latihan_bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment= FragmentFirst()
+        val secondFragment=FragmentSecond()
+        val thirdFragment= FragmentThird()
+
+        setCurrentFragment(firstFragment)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(firstFragment)
+                R.id.person->setCurrentFragment(secondFragment)
+                R.id.settings->setCurrentFragment(thirdFragment)
+
+            }
+            true
+        }
+
+    }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
+}
